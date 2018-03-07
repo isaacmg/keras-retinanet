6418d54ca8e3fea9480755be2b1ee13876e4ebc7
isaacmg <igodfried@isaac26.com>
2018-03-07 15:13:28 -0500

    switch to saving in debugger
    switched the annotations to save instead of display this should help debugging.

--- keras_retinanet/bin/debug.py
+++ keras_retinanet/bin/debug.py
@@ -156,7 +156,7 @@ def run(generator, args):
             labels, boxes, _ = generator.anchor_targets(image.shape, annotations, generator.num_classes())
             draw_boxes(image, boxes[np.max(labels, axis=1) == 1], (0, 255, 0))
 
-        cv2.imshow('Image', image)
+        cv2.imsave('file' + str(i) + '.png', image)
         if cv2.waitKey() == ord('q'):
             return False
     return True
