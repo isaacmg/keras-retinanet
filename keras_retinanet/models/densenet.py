239749fe811e87aed712dec68cdd5ff9b4b5e163
isaacmg <igodfried@isaac26.com>
2018-04-09 17:31:20 -0400

    update to default densenet master

--- keras_retinanet/models/densenet.py
+++ keras_retinanet/models/densenet.py
@@ -1,12 +1,9 @@
 """
 Copyright 2018 vidosits (https://github.com/vidosits/)
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,14 +16,15 @@ from keras.applications.densenet import DenseNet, get_file
 
 from ..models import retinanet
 
-WEIGHT_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.8/{}_weights_tf_dim_ordering_tf_kernels_notop.h5'
+origin = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.8/'
+file_name = '{}_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 custom_objects = retinanet.custom_objects
 
 allowed_backbones = {'densenet121': [6, 12, 24, 16], 'densenet169': [6, 12, 32, 32], 'densenet201': [6, 12, 48, 32]}
 
 
-def download_imagenet(backbone, weights_url=None):
+def download_imagenet(backbone):
     """ Download pre-trained weights for the specified backbone name. This name is in the format
         {backbone}_weights_tf_dim_ordering_tf_kernels_notop where backbone is the densenet + number of layers (e.g. densenet121).
         For more info check the explanation from the keras densenet script itself:
@@ -38,11 +36,9 @@ def download_imagenet(backbone, weights_url=None):
     # load weights
     if keras.backend.image_data_format() == 'channels_first':
         raise ValueError('Weights for "channels_first" format are not available.')
-    if weights_url is None:
-       weights_url = WEIGHT_PATH.format(backbone)
-    split_url = weights_url.split("/")
-    print("the split url is " + split_url[len(split_url)-1])
-    weights_path = get_file(split_url[len(split_url)-1], weights_url, cache_subdir='models')
+
+    weights_url = origin + file_name.format(backbone)
+    weights_path = get_file(file_name.format(backbone), weights_url, cache_subdir='models')
 
     return weights_path
 
@@ -57,7 +53,7 @@ def validate_backbone(backbone):
         raise ValueError('Backbone (\'{}\') not in allowed backbones ({}).'.format(backbone, list(allowed_backbones.keys())))
 
 
-def densenet_retinanet(num_classes, backbone='densenet121', modifier=None, inputs=None, **kwargs):
+def densenet_retinanet(num_classes, backbone='densenet121', inputs=None, modifier=None, **kwargs):
     # choose default input
     if inputs is None:
         inputs = keras.layers.Input((None, None, 3))
@@ -66,16 +62,16 @@ def densenet_retinanet(num_classes, backbone='densenet121', modifier=None, input
     densenet = DenseNet(blocks=blocks, input_tensor=inputs, include_top=False, pooling=None, weights=None)
 
     # get last conv layer from the end of each dense block
-    outputs = [densenet.get_layer(name='conv{}_block{}_concat'.format(idx + 2, block_num)).output for idx, block_num in enumerate(blocks)]
+    layer_outputs = [densenet.get_layer(name='conv{}_block{}_concat'.format(idx + 2, block_num)).output for idx, block_num in enumerate(blocks)]
 
     # create the densenet backbone
-    densenet = keras.models.Model(inputs=inputs, outputs=outputs, name=densenet.name)
-   
-    # invoke modifier if given
-    if modifier: 
-        densenet = modifier(densenet)
-        
-    # create the full model
-    model = retinanet.retinanet_bbox(inputs=inputs, num_classes=num_classes, backbone=densenet, **kwargs)
+    densenet = keras.models.Model(inputs=inputs, outputs=layer_outputs[1:], name=densenet.name)
 
-    return model
+    # invoke modifier if given
+    if modifier:
+        densenet = modifier(densenet)
+
+    # create the full model
+    model = retinanet.retinanet_bbox(inputs=inputs, num_classes=num_classes, backbone_layers=densenet.outputs, **kwargs)
+
+return model
